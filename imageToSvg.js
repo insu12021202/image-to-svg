78e6da907ab9df493b6a65d69125683c6c784894
Issac.yeo <insu12021202@gmail.com>
2023-02-28 11:01:18 +0900

    Fix: 주석 삭제

--- imageToSvg.js
+++ imageToSvg.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
 const pixelArtToSvg = require("pixel-art-2-svg");
-const fileSystem = require("fs");
 
 const dirName = "image";
 let arranges = [];
@@ -9,14 +8,15 @@ let arranges = [];
 let files = fs.readdirSync(dirName);
 console.log(files);
 
-//png 형식 검사 후 파일 이름만 arranges에 push
+//png, jpg 형식 검사 후 파일 이름만 arranges에 push
 files.forEach((file) => {
   let [filename, extension] = file.split(".");
-  if (String(extension) === "png") {
+  if (String(extension) === "png" || String(extension) === "jpg") {
     arranges.push(String(filename));
   }
 });
 
+//arranges의 파일을 svg로 변환하여 svg 폴더에 넣기
 arranges.forEach((files) => {
   pixelArtToSvg(dirName + `/${files}.png`).then((svgString) => {
     fs.writeFileSync("svg" + `/${files}.svg`, svgString, () =>
